2d9a988c3771544a63496f6f4dd4f0e58dc75d80
niyashiyas <98641481+niyashiyas@users.noreply.github.com>
2022-10-20 21:31:58 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -37,7 +37,8 @@ public class Calculator implements ActionListener{
         jf.setLocation(325, 50);
         Color color=new Color(65,16,16);
         jf.getContentPane().setBackground(color);
-        Color colour=new Color(247, 237, 225);
+        Color colour=new Color(247, 237, 225);        
+        displayLabel.setBackground(colour);
 
 
          
